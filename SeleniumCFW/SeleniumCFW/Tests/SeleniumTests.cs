721b376983d81909678f854252662c3f32492973
Lasitha Wijenayake <lasithdilshan20@gmail.com>
2023-12-18 11:38:29 +0800

    Add SeleniumTests.cs with initial test setup
    This commit adds a new file, SeleniumTests.cs, which includes the initial setup for a new test component. It sets the driver, maximizer, and path to HTML along with defining an initial test to fill information, select choices, and quit test after a pause.

---
+++ SeleniumCFW/SeleniumCFW/Tests/SeleniumTests.cs
@@ -0,0 +1,40 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using SeleniumCFW.Based;
+using SeleniumCFW.PageObjects;
+
+namespace  SeleniumCFW
+{
+    [TestFixture]
+    public class SeleniumTests
+    {
+        private IWebDriver driver;
+        private MainPage mainPage;
+        BaseCommand I = new BaseCommand();
+
+        [SetUp]
+        public void Setup()
+        {
+            driver = new ChromeDriver();
+            I.MaximizeWindow(driver);
+            I.Timeouts(driver,30);
+            string pathToHtml = @"C:\Automation\Selenium\SeleniumCFW\selenium-advance\SeleniumCFW\SeleniumCFW\HTML\index.html";
+            mainPage = new MainPage(driver);
+            mainPage.NavigateTo(pathToHtml);
+        }
+
+        [Test]
+        public void TestPage()
+        {
+            mainPage.FillName("Alexis Texas");
+            mainPage.FillEmail("a.texas@gmail.com");
+            
+            mainPage.SelectLanguage("Java");
+            mainPage.CheckOnCoding();
+
+            I.Pause(5000);
+            I.Quit(driver);
+        }
+    }
+}
