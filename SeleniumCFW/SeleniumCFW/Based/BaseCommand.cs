5ee3edf270b5bffde2da7ccfe5c49dae2318033e
Lasitha Wijenayake <lasithdilshan20@gmail.com>
2023-12-18 11:37:24 +0800

    Add SeleniumCFW project and page objects
    A new SeleniumCFW project has been added with relevant settings in csproj file. This setup includes the addition of NUnit, Selenium WebDriver and other dependencies. PageObjects and classes like MainPage and BaseCommand are introduced to encapsulate UI elements and handle commands, respectively. Also, HTML/index.html file has been moved to a new location under SeleniumCFW/HTML. An ignore rule has been added to .gitignore to exclude /SeleniumCFW/.idea/ directory.

---
+++ SeleniumCFW/SeleniumCFW/Based/BaseCommand.cs
@@ -0,0 +1,51 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+
+namespace SeleniumCFW.Based;
+
+public class BaseCommand
+{
+    public void Pause(int milliseconds)
+    {
+        Thread.Sleep(milliseconds);
+    }
+    
+    public void MaximizeWindow(IWebDriver driver)
+    {
+        driver.Manage().Window.Maximize();
+    }
+    
+    public void Click(IWebDriver driver, By by)
+    {
+        driver.FindElement(by).Click();
+    }
+    
+    public void SendKeys(IWebDriver driver, By by, String text)
+    {
+        driver.FindElement(by).SendKeys(text);
+    }
+    
+    public void SelectByText(IWebDriver driver, By by, String text)
+    {
+        var dropDown = driver.FindElement(by);
+        var selectElement = new SelectElement(dropDown);
+        selectElement.SelectByText(text);
+    }
+    
+    public void SelectByValue(IWebDriver driver, By by, String value)
+    {
+        var dropDown = driver.FindElement(by);
+        var selectElement = new SelectElement(dropDown);
+        selectElement.SelectByValue(value);
+    }
+    
+    public void Timeouts(IWebDriver driver, int seconds)
+    {
+        driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(seconds);
+    }
+    
+    public void Quit(IWebDriver driver)
+    {
+        driver.Quit();
+    }
+}
---
+++ SeleniumCFW/SeleniumCFW/PageObjects/MainPage.cs
@@ -0,0 +1,43 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+
+namespace SeleniumCFW.PageObjects;
+
+public class MainPage
+{
+    private readonly IWebDriver _driver;
+    private By TXT_Name => By.Id("name");
+    private By TXT_Email => By.Id("email");
+    
+    public MainPage(IWebDriver driver)
+    {
+        _driver = driver;
+    }
+    
+    public void NavigateTo(String url)
+    {
+        _driver.Navigate().GoToUrl(url);
+    }
+    
+    public void FillName(String name)
+    {
+        _driver.FindElement(TXT_Name).SendKeys(name);
+    }
+    
+    public void FillEmail(String email)
+    {
+        _driver.FindElement(TXT_Email).SendKeys(email);
+    }
+    
+    public void CheckOnCoding()
+    {
+        _driver.FindElement(By.Id("coding")).Click();
+    }
+    
+    public void SelectLanguage(String language)
+    {
+        var dropDownLanguage = _driver.FindElement(By.Id("dropdown"));
+        var selectElement = new SelectElement(dropDownLanguage);
+        selectElement.SelectByText(language);
+    }
+}
